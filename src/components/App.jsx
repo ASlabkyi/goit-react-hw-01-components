6344ff365aa3de8051ddaa9e9fc6a0fde8d7017c
Andrii Slabkyi <aslabkyi@gmail.com>
2023-03-19 16:50:39 +0200

    Complete task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 import user from '../components/Profile/user.json';
 import data from '../components/Statistics/data.json';
 import friends from '../components/FriendList/friends.json';
+import transactions from '../components/TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -18,6 +21,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
