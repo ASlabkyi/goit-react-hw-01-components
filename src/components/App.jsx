da6be0142e9da052034d9ce20d54bcece76e3cac
Andrii Slabkyi <aslabkyi@gmail.com>
2023-03-19 13:14:21 +0200

    Complete task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import user from '../components/Profile/user.json';
+import data from '../components/Statistics/data.json';
 
 export const App = () => {
   return (
-    <Profile
-      name={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
